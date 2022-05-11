a0553fbbb417457b7ff3b22ee8775a75baa03a21
GIGAWestbeekaw <westbeekaw999@gmail.com>
2022-05-11 20:47:08 +0700

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script>
+	import ScanQrCode from 'scan-qr-svelte/ScanQRCode.svelte';
+	function handleMessage(event) {
+		alert(event.detail.result);
+	}
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<ScanQrCode on:scanresult={handleMessage} />
+<h1>Reuslt :</h1>
